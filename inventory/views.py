77af7dd2901d7976a2bb9c40c9ad8fc4e602426c
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-07-02 09:26:49 +0500

    feat: add views logic

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Product, StockMovement
+from .serializers import CategorySerializer, ProductSerializer, StockMovementSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class StockMovementViewSet(viewsets.ModelViewSet):
+    queryset = StockMovement.objects.all()
+    serializer_class = StockMovementSerializer
